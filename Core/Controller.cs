0fd3c1bb525fe6c9f10f54641ce5eed5bfcb0b05
Vladislav <tsekov.vladislav@gmail.com>
2023-11-12 18:28:58 +0200

    Created a method for populating the "Years" table.

--- Core/Controller.cs
+++ Core/Controller.cs
@@ -1,19 +1,25 @@
-﻿namespace VehicleData.Core
+﻿using System.Text;
+using VehicleData.Data;
+using VehicleData.Data.Models;
+
+namespace VehicleData.Core
 {
     public class Controller
     {
         public void ReadData() 
         {
-            string filePath = @"all-vehicles-model.csv";
+            HashSet<Vehicle> cars = new();
 
+            string filePath = @"all-vehicles-model.csv";
             using var reader = new StreamReader(filePath);
             reader.ReadLine(); // Used to skip the first column - column's titles.
 
             while (!reader.EndOfStream)
             {
-                string? line = reader.ReadLine();
-                string[] data = line.Split(';');
+                string? currentCar = reader.ReadLine();
+                string[] carData = currentCar.Split(';');
 
+                VehicleDataContext? context = new();
             }
 
             reader.Dispose();
---
+++ Core/DataPopulation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleData.Data;
+using VehicleData.Data.Models;
+
+namespace VehicleData.Core
+{
+    public class DataPopulation
+    {
+        public void YearsTablePopulation() 
+        {
+            var context = new VehicleDataContext();
+
+            if (!context.Years.Any())
+            {
+                for (int year = 1984; year <= 2030; year++)
+                {
+                    context.Years.Add(new Year { ManufacturingYear = year });
+                }
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Years table is already populated. Verify the data in it.");
+            }
+        }
+    }
+}
