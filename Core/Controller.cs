dfdab121a3dbe6d52bfb905ce65d640264e99d10
Vladislav <tsekov.vladislav@gmail.com>
2023-11-12 17:48:20 +0200

    Started the creation of a StreamReader (.csv).

--- Core/Controller.cs
+++ Core/Controller.cs
@@ -2,5 +2,22 @@
 {
     public class Controller
     {
+        public void ReadData() 
+        {
+            string filePath = @"all-vehicles-model.csv";
+
+            using var reader = new StreamReader(filePath);
+            reader.ReadLine(); // Used to skip the title's column.
+
+
+            while (!reader.EndOfStream)
+            {
+                string? line = reader.ReadLine();
+                string[] data = line.Split(',');
+
+            }
+
+            reader.Dispose();
+        }
     }
 }
