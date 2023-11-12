67378ea9f53e4d5900bb6bc73ab93bbcdc9dea55
Vladislav <tsekov.vladislav@gmail.com>
2023-11-12 18:52:01 +0200

    Implemented changes into the main table - Vehicles.

--- Data/Models/Vehicle.cs
+++ Data/Models/Vehicle.cs
@@ -1,36 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace VehicleData.Data.Models;
-
-public partial class Vehicle
+namespace VehicleData.Data.Models
 {
-    public int Id { get; set; }
+    public partial class Vehicle
+    {
+        public int Id { get; set; }
 
-    public string? Make { get; set; }
-
-    public string? Model { get; set; }
-
-    public int? DrivetrainType { get; set; }
-
-    public int? TransmissionType { get; set; }
-
-    public int? VehicleSizeClass { get; set; }
-
-    public int? Year { get; set; }
-
-    public string? BaseModel { get; set; }
-
-
-
-
-    public virtual DrivetrainType? DrivetrainTypeNavigation { get; set; }
-
-    public virtual ICollection<Engine> Engines { get; set; } = new List<Engine>();
-
-    public virtual TransmissionType? TransmissionTypeNavigation { get; set; }
-
-    public virtual VehicleClass? VehicleSizeClassNavigation { get; set; }
-
-    public virtual Year? YearNavigation { get; set; }
+        public int? MakeId { get; set; }
+        public int? ModelId { get; set; }
+        public int? DrivetrainType { get; set; }
+        public int? TransmissionType { get; set; }
+        public int? VehicleSizeClass { get; set; }
+        public int? Year { get; set; }
+        public virtual VehicleMake Make { get; set; }
+        public virtual VehicleModel Model { get; set; }
+        public virtual VehicleBaseModel BaseModel { get; set; }
+        public virtual DrivetrainType? DrivetrainTypeNavigation { get; set; }
+        public virtual ICollection<Engine> Engines { get; set; } = new List<Engine>();
+        public virtual TransmissionType? TransmissionTypeNavigation { get; set; }
+        public virtual VehicleClass? VehicleSizeClassNavigation { get; set; }
+        public virtual Year? YearNavigation { get; set; }
+    }
 }
