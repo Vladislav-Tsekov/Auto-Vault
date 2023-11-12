84fd98ef2fb8bcf5716d9de66a1be8e7af62c89d
Vladislav <tsekov.vladislav@gmail.com>
2023-11-12 18:37:27 +0200

    Added a new table "VehicleModel".

---
+++ Data/Models/VehicleModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleData.Data.Models
+{
+    public class VehicleModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(75)]
+        public string Model { get; set; }
+
+        public virtual ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+    }
+}
