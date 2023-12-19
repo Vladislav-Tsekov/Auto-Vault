1c0c55fb453f50feb5fa153f81dae16abbd599e7
Vladislav <tsekov.vladislav@gmail.com>
2023-12-19 12:43:15 +0200

    Created StreamWriters for easier generation of SQL value input.

--- obj/Debug/net6.0/VehicleData.AssemblyInfo.cs
+++ obj/Debug/net6.0/VehicleData.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehicleData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8d2894dc59e041eacffb4e2c2ffc1013596a0c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehicleData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehicleData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
